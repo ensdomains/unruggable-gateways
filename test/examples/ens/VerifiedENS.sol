a21a0e0b3364bc897342b54d191b2135b63e127e
Andrew Raffensperger <raffy@me.com>
2025-07-29 20:31:17 -0700

    cleanup contract

--- test/examples/ens/VerifiedENS.sol
+++ test/examples/ens/VerifiedENS.sol
@@ -8,55 +8,46 @@ contract VerifiedENS is GatewayFetchTarget {
     using GatewayFetcher for GatewayRequest;
 
     address constant ENS_REGISTRY = 0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e;
-    address constant PUBLIC_RESOLVER_V3 =
-        0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63;
 
     IGatewayVerifier immutable verifier;
-    mapping(address resolver => bytes program) programs;
+    //mapping(address resolver => bytes program) programs;
 
     constructor(IGatewayVerifier _verifier) {
         verifier = _verifier;
     }
 
-
-
-    function resolveWithProgram(
-        bytes32 node,
-		bytes calldata program
-    ) external view returns (address, bytes memory) {
-        GatewayRequest memory req = GatewayFetcher.newRequest(2);
-        req.push(node); // put node at stack(0)
-        req.setTarget(ENS_REGISTRY); // target registry
-        req.dup().follow().offset(1).read().setOutput(0); // read resolver and save it
-		req.pushOutput(0).target(); // target resolver
-		req.push(program); // load program
-		req.eval(); // call it
-        fetch(verifier, req, this.resolveCallback.selector);
-    }
-    
-	// function setResolverProgram(
+    // function setResolverProgram(
     //     address resolver,
     //     bytes calldata program
     // ) external {
     //     programs[resolver] = program;
     // }
 
+    function resolveWithProgram(
+        bytes32 node,
+        bytes calldata program
+    ) external view returns (address, bytes memory) {
+        GatewayRequest memory req = GatewayFetcher.newRequest(2);
+        req.push(node); // put node at stack(0)
+        req.setTarget(ENS_REGISTRY); // target registry
+        req.dup().follow().offset(1).read().setOutput(0); // read resolver and save it
+        req.push(program); // load program
+        req.pushOutput(0).target(); // target resolver
+        req.eval(); // call it
+        fetch(verifier, req, this.resolveCallback.selector);
+    }
+
     // function resolve(
     //     bytes32 node
     // ) external view returns (address, bytes memory) {
     //     GatewayRequest memory req = GatewayFetcher.newRequest(2);
-
-    //     req.push(node);
-
-    //     // get resolver
-    //     req.setTarget(ENS_REGISTRY);
-    //     req.dup().follow().offset(1).read().setOutput(0);
-
-    //     // read program
-    //     req.setTarget(address(this));
-    //     req.pushOutput(0).follow().readBytes();
-    //     req.eval();
-
+    //     req.push(node); // put node at stack(0)
+    //     req.setTarget(ENS_REGISTRY); // target registry
+    //     req.dup().follow().offset(1).read().setOutput(0); // read resolver and save it
+    //     req.setTarget(address(this)); // target self
+    //     req.pushOutput(0).follow().readBytes(); // load program
+    //     req.pushOutput(0).target(); // target resolver
+    //     req.eval(); // call it
     //     fetch(verifier, req, this.resolveCallback.selector);
     // }
 
